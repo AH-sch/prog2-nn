4ca81e04f6b9471667af81ed2ddcd8e9b619028c
Asano Hayato <j3304uxn@sit.ac.jp>
2024-12-17 16:39:03 +0900

    デバイスへの転送機能をつける　改

--- models.py
+++ models.py
@@ -25,12 +25,12 @@ def test_accuracy(model, dataloader, device='cpu'):
     #全てのミニバッチに対して推論して、正解率を計算する
     n_corrects = 0 # 正解の個数
 
-    model.to(device)
+    model = model.to(device)
 
     model.eval()
     for image_batch, label_batch in dataloader:
-        image_batch.to(device)
-        label_batch.to(device)
+        image_batch = image_batch.to(device)
+        label_batch = label_batch.to(device)
         # モデルに入れて結果(logits)を出す
         with torch.no_grad():
             logits_batch = model(image_batch)
@@ -46,12 +46,12 @@ def test_accuracy(model, dataloader, device='cpu'):
 
 def train(model, dataloader, loss_fn, optimizer, device='cpu'):
     """1 epoch の学習を行う"""
-    model.to(device)
+    model = model.to(device)
     model.train()
     for image_batch, label_batch in dataloader:
         # モデルにバッチを入れて計算
-        image_batch.to(device)
-        label_batch.to(device)
+        image_batch = image_batch.to(device)
+        label_batch = label_batch.to(device)
 
         logits_batch = model(image_batch)
 
@@ -69,12 +69,12 @@ def train(model, dataloader, loss_fn, optimizer, device='cpu'):
 def test(model, dataloader, loss_fn, device='cpu'):
     loss_total = 0.0
 
-    model.to(device)
+    model = model.to(device)
     model.eval()
     for image_batch, label_batch in dataloader:
-        image_batch.to(device)
-        label_batch.to(device)
-        
+        image_batch = image_batch.to(device)
+        label_batch = label_batch.to(device)
+
         with torch.no_grad():
             logits_batch = model(image_batch)
 
